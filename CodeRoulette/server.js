51ca8a6eba3f0f71e7494831a796dc1c1cb999c5
mkmvrm <msandstrom@pipeline.sbcc.edu>
2016-04-23 07:14:24 -0400

    Commit#3

--- CodeRoulette/server.js
+++ CodeRoulette/server.js
@@ -1,8 +1,27 @@
 var express = require("express");
 var app = express();
 app.use(express.static(__dirname + '/public'));
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
 var router = express.Router();
 var path = __dirname + '/views/';
+
+// POST http://localhost:8080/api/users
+// parameters sent with 
+app.post('/main.html', function(req, res) {
+    var usr_name = req.body.user;
+    var usr_password = req.body.pass;
+
+    res.send(usr_name + ' ' + usr_password);
+});
+
+app.post('/main.html', function(req, res) {
+    res.send('Us@@@ername: ' + req.body.user);
+});
+
 
 router.use(function (req,res,next) {
   console.log("/" + req.method);
